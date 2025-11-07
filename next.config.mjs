f55bebafd01bb01ce4102a849ae64f8f61a7a56f
f5yc9n7ygb-web <f5yc9n7ygb@privaterelay.appleid.com>
2025-11-07 22:18:46 +0530

    Update next.config.mjs with new configuration

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   experimental: { typedRoutes: true },
-  output: 'standalone',
   images: {
     unoptimized: true, // 👈 serve from /public directly (no optimizer)
   },
